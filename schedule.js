49671d84718a1d2a50a4fc2d657c46b62e3debc7
NakWon Park <own1019@naver.com>
2020-11-03 19:55:58 +0900

    제거

--- schedule.js
+++
@@ -1,6 +0,0 @@
-const schedule = require('node-schedule');
-	let job = schedule.scheduleJob('15 * * * * *', function(){
-		let mNow = new Date();
-        console.log('excute time : ' + mNow);
-        require('./index.js');
-	});
