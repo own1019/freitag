243037436ae3e9d3ad3132031df19ff6d9b4e600
NakWon Park <own1019@naver.com>
2020-11-12 15:53:42 +0900

    commit

--- telegramBot.js
+++ telegramBot.js
@@ -5,9 +5,8 @@ const CHAT_ID = '1348047190';
 const bot = new TelegramBot(token, {polling: true});
 const text = '프라이탁 제품이 업데이트 되었습니다.';
 
-async function telegramBot(imgUrl) {
+module.exports = async function telegramBot(imgUrl) {
     console.log('텔레그램 메세지 발송!');
     await bot.sendMessage(CHAT_ID, text);
     await bot.sendPhoto(CHAT_ID, imgUrl);
 }
-exports.telegramBot = telegramBot;
